2bc6fcdac741726b7a4dc87f636982c6d2b6a356
Sebas5158R <sebastianriveraa4@gmail.com>
2026-04-16 16:23:07 -0500

    Add 'terceros' app with URL routing, views, and templates

---
+++ supermercado/terceros/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'terceros'
+
+urlpatterns = [
+    path('clientes/<str:nombre>/<str:activo>/<str:fecha_registro>/', views.clientes, name='clientes'),
+    path('inicio/', views.inicio, name='inicio'),
+    path('saludo_cliente/<int:cliente_id>/', views.saludo_cliente, name='saludo_cliente'),
+    path('lista_clientes/<int:cliente_id>/', views.lista_clientes, name='lista_clientes'),
+]
--- supermercado/terceros/views.py
+++ supermercado/terceros/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
 # Create your views here.
+def clientes(request, nombre, activo, fecha_registro):
+    return HttpResponse(f"<h1 style='color: red; display: flex; justify-content: center; text-align: center;'>Bienvenido a nuestro supermercado:<span style='color: black;'>{nombre}</span></h1> <h1 style='display: flex; justify-content: center; text-align: center;'>Estado actual: <span style='color: green;'>{activo}</span></h1> <p style='color: black; display: flex; justify-content: center; text-align: center;'>Fecha de Registro: {fecha_registro}</p> <label style='color: black; display: flex; justify-content: center; text-align: center;'>Buscar producto</label> <input type='search' style='display: flex; justify-content: center; text-align: center; margin: 0 auto; padding: 10px; border-radius: 5px; border: 1px solid #ccc;'> <br/> <button style='display: flex; justify-content: center; text-align: center; margin: 0 auto; padding: 10px 20px; background-color: green; color: white; border: none; border-radius: 5px; cursor: pointer;'>Buscar</button> <h1 style='color: black; display: flex; justify-content: center; text-align: center;'>Productos disponibles</h1> <div style='display: flex; justify-content: center; text-align: center;'> <img style='width: 300px; height: 300px;' src='https://imgs.search.brave.com/qE6SHraEVE8BA6TOYPjpBwluas_ZgNwBYDS1b_VjSkU/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNzIv/MTE4Lzg2OC9zbWFs/bC9yZWQtYW5kLWdy/ZWVuLWFwcGxlcy13/aXRoLWZyZXNoLXdh/dGVyLWRyb3BzLXBo/b3RvLmpwZw' alt='Manzanas'/> <img style='width: 300px; height: 300px;' src='https://imgs.search.brave.com/vnDGMF-OkJFoDX0kLVQuAqwPupjuy2eSjETLhk6xeAs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMzkv/NzYzLzg2MS9zbWFs/bC9haS1nZW5lcmF0/ZWQtcmlwZS1tYW5n/by1mcnVpdC1iYWNr/Z3JvdW5kLXBob3Rv/LmpwZw' alt='Mangos'/> <img style='width: 300px; height: 300px;' src='https://imgs.search.brave.com/rEEuYpOxJ7LggwHEOwiL0ApjT3FfmYoa2NfjwENKOpM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy82/LzYwL0NvcnRlX3Ry/YW5zdmVyc2FsX2Zy/ZXNhLmpwZw' alt='Fresas'/></div> <br/><br/> <h1 style='color: black; display: flex; justify-content: center; text-align: center;'>Gracias por visitarnos</h1>")
+
+def inicio(request):
+    data = {
+        "usuario": "Juan Perez",
+        "email": "juan@gmail.com"
+    }
+    return render(request, "index.html", data)
+
+def saludo_cliente(request, cliente_id):
+    data = models.Cliente.objects.get(id=cliente_id)
+    # Se puede usar Try Catch para manejar el error en caso de que no exista el cliente
+    return render(request, "saludo_cliente.html", {"cliente": data})
+
+def lista_clientes(request, cliente_id):
+    cliente_actual = models.Cliente.objects.get(id=cliente_id)
+    clientes = models.Cliente.objects.all()
+    return render(request, "lista_clientes.html", {"clientes": clientes, "cliente_actual": cliente_actual})
