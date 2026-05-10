aba824a9db74909531c9017bd16303dfa472b980
Johanbeltranramirez <johanframirez13@gmail.com>
2026-05-10 15:20:12 -0500

    Adelanto en Reportes

--- supermercado/reportes/urls.py
+++ supermercado/reportes/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'reportes'
+
 urlpatterns = [
-    path('list/', views.list),
-    path('reportes/<str:tipo>', views.reportes),
-    path('config/<uuid:id_reporte>/', views.config)
+    path('list/', views.list, name='list'),
+    path('crear/', views.crear, name='crear'),
+    path('editar/<uuid:id_reporte>/', views.editar, name='editar'),
+    path('eliminar/<uuid:id_reporte>/', views.eliminar, name='eliminar'),
+    path('config/<uuid:id_reporte>/', views.config, name='config'),
+    path('ver/<str:tipo>/', views.reportes, name='ver'),
 ]
--- supermercado/supermercado/urls.py
+++ supermercado/supermercado/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reportes/', include('reportes.urls'))
+    path('reportes/', include('reportes.urls', namespace='reportes')),
 ]
