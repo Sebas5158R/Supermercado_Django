c8d910c5cd84a8c5ed62987ffa3f9589029f39fa
Johanbeltranramirez <johanframirez13@gmail.com>
2026-05-10 19:06:07 -0500

    Adelanto en Reportes, faltan pruebas integrales con los demás módulos

--- supermercado/reportes/urls.py
+++ supermercado/reportes/urls.py
@@ -4,10 +4,12 @@ from . import views
 app_name = 'reportes'
 
 urlpatterns = [
-    path('list/', views.list, name='list'),
+    path('list/', views.listar, name='listar'),
     path('crear/', views.crear, name='crear'),
     path('editar/<uuid:id_reporte>/', views.editar, name='editar'),
     path('eliminar/<uuid:id_reporte>/', views.eliminar, name='eliminar'),
     path('config/<uuid:id_reporte>/', views.config, name='config'),
     path('ver/<str:tipo>/', views.reportes, name='ver'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('categorias/eliminar/<int:categoria_id>/', views.eliminar_categoria, name='eliminar_categoria'),
 ]
