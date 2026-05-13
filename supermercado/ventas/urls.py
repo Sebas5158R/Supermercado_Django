80825f1100ffc7d8f4a7ce422fef99d833aa944f
shadowsouls444 <angel.acuna.meza@gmail.com>
2026-05-13 15:57:31 -0500

    lista para ver detalle venta listo

--- supermercado/ventas/urls.py
+++ supermercado/ventas/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'ventas'
 
 urlpatterns = [
-    path('lista_ventas/', views.getAllVentas, name='lista_ventas'),
-    path('crear_venta/', views.template_formulario, name='crear_venta')
+    path('lista_ventas/', views.getVentas, name='lista_ventas'),
+    path('crear_venta/', views.template_formulario, name='crear_venta'),
+    path('detalle_venta/<int:venta_id>/', views.getDetalle, name='detalle_venta'),
 ]
--- supermercado/ventas/views.py
+++ supermercado/ventas/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from . import models
 
 def template_formulario(request):
     return render(request, 'formulario_venta.html')
 
-def getAllVentas(request):
-    return render(request, 'lista_ventas.html')
+def getDetalle(request, venta_id):
+
+    detalles_venta = models.DetalleVenta.objects.filter(venta_id=venta_id)
+    venta = models.Venta.objects.get(id=venta_id)
+
+    data = {
+        'venta': venta,
+        'detalles_venta': detalles_venta
+    }
+
+    return render(request, 'detalle_venta.html', data)
+
+def getVentas(request):
+
+    ventas = models.Venta.objects.all()
+
+    data = {
+        'ventas': ventas
+    }
+
+    return render(request, 'lista_ventas.html', data)
